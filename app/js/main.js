1a789f9c157db9e2c61302f74f6e6b92c4af3612
Miguel Zablah <miguel12979@gmail.com>
2018-09-10 22:47:37 -0500

    Minify js for functions

--- app/js/main.js
+++ app/js/main.js
@@ -4,13 +4,13 @@ function calcTrivia() {
 	triviaRes.forEach(i => { count[i] = (count[i]||0) + 1;});
 
 	let max = Object.keys(count).reduce((key, val) => count[key] > count[val] ? key : val);
-	
-	document.getElementById("trivia").style.display = "none";
+
+		document.getElementById("trivia").style.display = "none";
 	document.getElementById("questions__results").style.display = "block";
-	
-	let resObj = proximus.getKeyValue(langObj, 'respuestas');
-	
-	if(max === 'a'){
+
+		let resObj = proximus.getKeyValue(langObj, 'respuestas');
+
+		if(max === 'a'){
 		document.getElementById("triviaTitle").innerHTML = resObj.A.titulo;
 		document.getElementById("triviaText").innerHTML = resObj.A.text;
 	}else if(max === 'b'){
